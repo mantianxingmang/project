09c1a73bec6d80eec1adebf08fd7dfc04a779e4f
ZY <1375020779@qq.com>
2020-02-27 22:51:07 +0800

    添加了数据库支持扩展Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,8 +1,45 @@
-from flask import Flask,render_template
-app = Flask(__name__)
+import os
+import sys
+import click
 
-@app.route('/')
-def index():
+from flask import Flask,render_template
+
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #关闭对模型修改的监控
+db = SQLAlchemy(app)
+
+
+#注册命令
+@app.cli.command()
+@click.option('--drop',is_flag=True,help='Create after drop')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('初始化数据库。')
+
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True) #主键
+    name = db.Column(db.String(20)) #name
+
+class Movie(db.Model):
+    id =db.Column(db.Integer,primary_key=True) #主键
+    title = db.Column(db.String(60)) #电影标题
+    year = db.Column(db.String(4)) #电影年份
+
+@app.cli.command()
+def forge():
+    db.create_all()
     name = "ZY"
     movies = [
         {'title':'杀破狼','year':'2003'},
@@ -16,4 +53,18 @@ def index():
         {'title':'导火索','year':'2005'},
         {'title':'叶问','year':'2015'}
     ]
-    return render_template('index.html',name=name,movies=movies)
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('导入数据完成')
+
+
+@app.route('/')
+def index():
+
+    user = User.query.first()
+    movies = Movie .query.all()
+    return render_template('index.html',user=user,movies=movies)
